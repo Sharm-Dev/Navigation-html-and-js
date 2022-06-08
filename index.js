005b965a9fd3765d1a95b0c89d7eb8fafe14e836
Sharmaine <sharmainetaylor@ymail.com>
2022-06-08 21:46:04 +0100

    Created two variables and used get element by classname to attach the variables to the classess in html, the added a event listener to the hamburger variable that responds to a click and calls a function called mobileNav

---
+++ index.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementsByClassName(".hamburger");
+const navContainer = document.getElementsByClassName(".nav-container");
+
+hamburger.addEventListener("click", mobileNav);
+
+function mobileNav() {
+   
+}
