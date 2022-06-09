690c283270da546a487457289efe89aeeb534cda
Sharmaine <sharmainetaylor@ymail.com>
2022-06-09 22:24:27 +0100

    change event listener on hamburger to be active when the browser width reaches less then 700px then a function activates the nav contanier styles

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const hamburger = document.getElementsByClassName(".hamburger");
-const navContainer = document.getElementsByClassName(".nav-container");
+const hamburger = document.getElementById("hamburger");
+const navContainer = document.getElementById("nav-container");
+console.log(navContainer);
 
 hamburger.addEventListener("click", mobileNav);
+console.log(window.innerWidth);
+if (window.innerWidth < 700) {
+    navContainer.classList.toggle("active");
+}
 
 function mobileNav() {
-   
+    console.log("I work");
+    //hamburger.classList.toggle("active")
+    navContainer.classList.toggle("active");
+}
+
+const navList = document.querySelectorAll(".nav-list");
+
+navList, forEach(item => item.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navContainer.classList.remove("active");
 }
